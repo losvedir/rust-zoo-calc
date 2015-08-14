068e35f5fd8e396c832973100b12917fc9b55397
Gabe Durazo <gabe@gabedurazo.com>
2015-08-14 12:05:13 +0800

    Impl fmt::Display for Expr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 enum Expr {
     Numeral(i64),
@@ -8,7 +10,30 @@ enum Expr {
     Negate(Box<Expr>),
 }
 
+impl fmt::Display for Expr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            &Expr::Numeral(i) => write!(f, "{}", i),
+            &Expr::Plus(ref i, ref j) => write!(f, "{} + {}", i, j),
+            &Expr::Minus(ref i, ref j) => write!(f, "{} - {}", i, j),
+            &Expr::Times(ref i, ref j) => write!(f, "{} * {}", i, j),
+            &Expr::Divide(ref i, ref j) => write!(f, "{} / {}", i, j),
+            &Expr::Negate(ref i) => write!(f, "-{}", i),
+        }
+    }
+}
+
 fn main() {
     let n1: Expr = Expr::Numeral(5);
-    println!("{:?}", n1);
+    let p: Expr = Expr::Plus(Box::new(Expr::Numeral(5)), Box::new(Expr::Numeral(7)));
+    let m: Expr = Expr::Minus(Box::new(Expr::Numeral(5)), Box::new(Expr::Numeral(7)));
+    let t: Expr = Expr::Times(Box::new(Expr::Numeral(5)), Box::new(Expr::Numeral(7)));
+    let d: Expr = Expr::Divide(Box::new(Expr::Numeral(5)), Box::new(Expr::Numeral(7)));
+    let n: Expr = Expr::Negate(Box::new(Expr::Numeral(5)));
+    println!("{}", n1);
+    println!("{}", p);
+    println!("{}", m);
+    println!("{}", t);
+    println!("{}", d);
+    println!("{}", n);
 }
